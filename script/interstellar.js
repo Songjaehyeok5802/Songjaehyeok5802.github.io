001845e4d25fa67fdd89c21ccabc936de4b30472
Song jae hyeok <wakij6587@naver.com>
2020-11-05 20:52:42 +0900

    서브페이지추가 및 디자인 수정
    아직 디자인 진행중입니다.

---
+++ script/interstellar.js
@@ -0,0 +1,13 @@
+
+
+const checkpoint = window.innerHeight - 500;
+window.addEventListener("scroll", () => {
+  const currentScroll = window.scrollY;
+  if (currentScroll <= checkpoint) {
+    opacityG = 1 - currentScroll / checkpoint;
+  } else {
+    opacityG = 0;
+  }
+  document.querySelector(".title").style.opacity = opacityG;
+});
+
