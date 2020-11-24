9e6799251fd73bdb3af34d26cc47e541f0278e77
Song jae hyeok <wakij6587@naver.com>
2020-11-24 21:24:53 +0900

    lab 페이지 링크 수정
    lab 페이지 링크 수정

--- lab/ChangeColorByTxt/script/script.js
+++ lab/ChangeColorByTxt/script/script.js
@@ -15,6 +15,7 @@ function three(){
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
     const renderer = new THREE.WebGLRenderer({ alpha: true });
     document.getElementById("obj").appendChild(renderer.domElement);
+    control = new THREE.OrbitControls(camera, renderer.domElement);
 
     renderer.setSize(window.innerWidth, window.innerHeight);
     window.addEventListener("resize", function(){
@@ -42,7 +43,6 @@ function three(){
     scene.add(cube);
 
     let matColor;
-    console.log(Math.random() * 100);
     const renderScene = new function renderScene() {
         requestAnimationFrame(renderScene);
         camera.lookAt(0, 0, 0);  
@@ -60,6 +60,8 @@ function three(){
     
 }
 three();
+
+
 
 
 
