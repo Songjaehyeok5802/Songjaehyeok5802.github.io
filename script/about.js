92f34efdae3fe38c6f1060e327d7795de9f72305
Song jae hyeok <wakij6587@naver.com>
2020-12-04 14:26:12 +0900

    about 페이지 스크롤이벤트 추가(보류)
    스크롤 이벤트를 추가하였지만 과하다는 느낌이 들어 주석처리 하였습니다.

--- script/about.js
+++ script/about.js
@@ -11,67 +11,121 @@ new hoverEffect({
 });
 
 
+//three 
+/*
+    function bg_3D() {
+        const scene = new THREE.Scene();
+        const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+        const renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true});
+        document.getElementById("bg_3D").appendChild(renderer.domElement);
+        // control = new THREE.OrbitControls(camera, renderer.domElement);
 
-function bg_3D() {
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-    const renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true});
-    document.getElementById("bg_3D").appendChild(renderer.domElement);
-    // control = new THREE.OrbitControls(camera, renderer.domElement);
+        //SIZE ---------
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        window.addEventListener("resize", function(){
+            const width = window.innerWidth;
+            const height = window.innerHeight;
+            renderer.setSize(width, height);
+            camera.aspect = width/height;
+            camera.updateProjectionMatrix();
+        })
+        renderer.setClearColor( 0x000000, 0 );
 
-    //SIZE ---------
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    window.addEventListener("resize", function(){
-        const width = window.innerWidth;
-        const height = window.innerHeight;
-        renderer.setSize(width, height);
-        camera.aspect = width/height;
-        camera.updateProjectionMatrix();
-    })
-    renderer.setClearColor( 0x000000, 0 );
+        //CAMERA--------
+        camera.position.set(2.5, 0, 0); //0, 0.5, 3
+        camera.lookAt(scene.position);  
 
-    //CAMERA--------
-    camera.position.set(3, 0, 0); //0, 0.5, 3
-    camera.lookAt(scene.position);  
+        //LIGHT-------------
+        const hemiLight = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
+        scene.add( hemiLight );
 
-    //LIGHT-------------
-    const SpotlightDown = new THREE.PointLight( 0xffffff, 1.5); 
-    const SpotlightLeft = new THREE.PointLight( 0xe1fcff, 3); 
-    const SpotlightRight = new THREE.PointLight( 0xf6ffe1, 3); 
-    const SpotlightUp = new THREE.PointLight( 0xffffff, 1.5);  
-    scene.add( SpotlightDown );
-    scene.add( SpotlightLeft );
-    scene.add( SpotlightRight );
-    scene.add( SpotlightUp );
-    SpotlightDown.position.set(0, -4, 0);
-    SpotlightLeft.position.set(0, 0, 3);
-    SpotlightRight.position.set(0, 0, -3);
-    SpotlightUp.position.set(0, 4, 0);
+        const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.3 );
+        directionalLight.position.set(0, 0, 5);
+        scene.add( directionalLight );
 
-    var geo = new THREE.SphereGeometry( 1.5, 40, 40);
-    const mat = new THREE.MeshLambertMaterial({color : 0xedfaff, wireframe : false});
-    const box = new THREE.Mesh(geo, mat);
-    box.position.set(0, 0, 0);
-    scene.add(box);
+        const geo = new THREE.BoxGeometry( 1, 1, 1 );
+        const matGroup = [
+            new THREE.MeshBasicMaterial({map : new THREE.TextureLoader().load("../img/logo/html.png"), side : THREE.DoubleSide}),
+            new THREE.MeshBasicMaterial({map : new THREE.TextureLoader().load("../img/logo/css.png"), side : THREE.DoubleSide}),
+            new THREE.MeshBasicMaterial({map : new THREE.TextureLoader().load("../img/logo/js.png"), side : THREE.DoubleSide}),
+            new THREE.MeshBasicMaterial({map : new THREE.TextureLoader().load("../img/logo/jquery.png"), side : THREE.DoubleSide}),
+            new THREE.MeshBasicMaterial({map : new THREE.TextureLoader().load("../img/logo/three.png"), side : THREE.DoubleSide}),
+            new THREE.MeshBasicMaterial({map : new THREE.TextureLoader().load("../img/logo/"), side : THREE.DoubleSide})
+        ];
+        const toolMat = new THREE.MeshFaceMaterial(matGroup);
+        const box = new THREE.Mesh(geo, toolMat);
+        box.position.set(0, 0, 1.5);
+        box.rotation.set(0, 5.1, 0);
+        scene.add(box);
+        let scrollValue= 0;
+
+        const body = document.querySelector("body");
+        const bodyH = body.offsetHeight;
+        console.log(bodyH);
 
 
-    //RENDER-------------------------------------------------------------------------------
-    const renderScene = new function renderScene() {
-        requestAnimationFrame(renderScene);
-
-        if(camera.position.x > 0){
-            camera.position.x -=0.01;
+        function boxAni(){
+            this.tl = new TimelineMax();
+            let stan = bodyH / 8;
+            $(window).scroll( () => {
+                scrollValue = $(document).scrollTop();
+                return scrollValue;
+            });
+            // console.log(scrollValue); 
+            if(scrollValue < stan){
+                $("article.introduce").css({"opacity" : 1});
+                $("div.toolTxt").css({"opacity" : 0});
+                $("p.greet").css({"opacity" : 0});
+                this.tl.to(box.rotation,2, {x: 0, y : 5.1, z : 0,  ease : Expo.easeOut})
+            }
+            if(scrollValue > stan && scrollValue <= stan * 2){
+                $("article.introduce").css({"opacity" : 0});
+                $("div.toolTxt").css({"opacity" : 0}).eq(0).css({"opacity" : 1});
+                $("p.greet").css({"opacity" : 0});
+                this.tl.to(box.rotation,2, { x : 0, y : 0.7,  z : 0, ease : Expo.easeOut})
+            }
+            if(scrollValue > stan * 2 && scrollValue <= stan * 3){
+                $("article.introduce").css({"opacity" : 0});
+                $("div.toolTxt").css({"opacity" : 0}).eq(1).css({"opacity" : 1});
+                this.tl.to(box.rotation,2, { x : 0, y : 4,  z : 0, ease : Expo.easeOut})
+            }
+            if(scrollValue > stan * 3 && scrollValue <= stan * 4){
+                $("article.introduce").css({"opacity" : 0});
+                $("div.toolTxt").css({"opacity" : 0}).eq(2).css({"opacity" : 1});
+                $("p.greet").css({"opacity" : 0});
+                this.tl.to(box.rotation,2, {x: -1.5, y : -3.3, z : 1,  ease : Expo.easeOut})
+                
+            }
+            if(scrollValue > stan * 4 && scrollValue <= stan * 5){
+                $("article.introduce").css({"opacity" : 0});
+                $("div.toolTxt").css({"opacity" : 0}).eq(3).css({"opacity" : 1});
+                $("p.greet").css({"opacity" : 0});
+                this.tl.to(box.rotation,2, {x: 1.5, y : 3.1, z : -1,  ease : Expo.easeOut})
+            }
+            if(scrollValue > stan * 5 && scrollValue <= stan * 6){
+                $("article.introduce").css({"opacity" : 0});
+                $("div.toolTxt").css({"opacity" : 0}).eq(4).css({"opacity" : 1});
+                $("p.greet").css({"opacity" : 0});
+                this.tl.to(box.rotation,2, {x : 0, y : 2, z : 0, ease : Expo.easeOut});
+            }
+            if(scrollValue > stan * 6){
+                $("article.introduce").css({"opacity" : 0});
+                $("div.toolTxt").css({"opacity" : 0});
+                $("p.greet").css({"opacity" : 1});
+                this.tl.to(box.rotation,2, {x : 0, y : 5.1, z : 0, ease : Expo.easeOut});
+                this.tl.to(box.scale, 0.1, {x : 0, y : 0, z : 0, ease : Expo.easeOut});
+            }else{
+                this.tl.to(box.scale, 2, {x : 1, y : 1, z : 1, ease : Expo.easeOut});
+            }
         }
-        if(camera.position.y < 0.5){
-            camera.position.y +=0.01;
-        }
-        if(camera.position.z < 3){
-            camera.position.z +=0.01;
-        }
-        camera.lookAt(0, 0, 0);
 
-        box.rotation.y += 0.002;
-        renderer.render(scene,camera);
-    }   
-}
-// bg_3D();
+        //RENDER-------------------------------------------------------------------------------
+        const renderScene = new function renderScene() {
+            requestAnimationFrame(renderScene);
+
+
+            renderer.render(scene,camera);
+        }   
+    }
+    bg_3D();
+*/
